e6722792356efcf527febbeda44684caebd15e51
Massis <massis200936@gmail.com>
2018-08-21 22:42:40 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/SummerPractic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SummerPractic.dir/main.cpp.obj"
+  "SummerPractic.pdb"
+  "SummerPractic.exe"
+  "SummerPractic.exe.manifest"
+  "libSummerPractic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SummerPractic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
